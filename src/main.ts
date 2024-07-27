95fcf7127a59a564c97f1ab2ec529153972e54de
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-07-27 10:29:32 -0300

    tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
+import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
 const app = createApp(App)
 
 app.use(createPinia())
