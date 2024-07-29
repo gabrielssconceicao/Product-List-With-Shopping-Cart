142cb5e6e63ba76eb6584d7eed6015ba12d1dc26
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-07-29 12:51:04 -0300

    showing products on confirm order

---
+++ src/components/ConfirmOrder/ConfirmOrderItemComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { CartItem } from '@/stores/cart';
+type ConfirmOrderItemProps = {
+  product: CartItem
+}
+const props = defineProps<ConfirmOrderItemProps>()
+</script>
+
+<template>
+  <div class="flex flex-row items-stretch gap-3 px-2
+  ">
+    <div class="w-20">
+      <img :src="props.product.image" :alt="`${props.product.name} thumbnail`">
+    </div>
+    <div class="flex-1 flex-grow bg-red">
+      <p>{{ props.product.name }}</p>
+      <div>
+        <p>x{{ props.product.quantity }}</p>
+        <p>${{ props.product.price.toFixed(2) }}</p>
+      </div>
+    </div>
+    <p>${{ (props.product.price * props.product.quantity).toFixed(2) }}</p>
+  </div>
+
+
+</template>
--- src/views/ConfirmOrderView.vue
+++ src/views/ConfirmOrderView.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import ConfirmOrderItem from '@/components/ConfirmOrder/ConfirmOrderItemComponent.vue'
+import { useCartStore } from '@/stores/cart';
+const cartStore = useCartStore();
 const emit = defineEmits(['closeConfirmOrderModal']);
 
 const handleCloseConfirmOrderModal = () => {
@@ -7,9 +10,29 @@ const handleCloseConfirmOrderModal = () => {
 </script>
 
 <template>
-  <section class="fixed top-0 bg-black/50 w-full h-full ">
+  <section class="fixed top-0 bg-black/50 w-full h-full space-y-5">
 
-    <h1>Confirm Order</h1>
-    <button @click="handleCloseConfirmOrderModal">Close</button>
+    <section
+      class="bg-white absolute flex flex-col mobile:w-full mobile:bottom-0 mobile:rounded-t-2xl px-3 py-5 h-[90%] font-red-hat-text shadow-lg">
+      <div class="space-y-5">
+        <div>
+          <img src="../assets/images/icon-order-confirmed.svg" alt="order-confirmed">
+        </div>
+        <h2 class="font-red-hat-text font-bold text-4xl text-rose-900 inline-flex flex-col gap-3">
+          <span>Order</span>
+          <span>Confirmed</span>
+        </h2>
+        <p class="text-rose-500">Hope you enjoy your food</p>
+      </div>
+
+      <div class="flex-1 overflow-y-auto space-y-1 py-1">
+        <ConfirmOrderItem v-for="(product, i) in cartStore.cart" :key="i" :product="product" />
+      </div>
+
+
+      <button @click="handleCloseConfirmOrderModal"
+        class=" bg-red px-10 py-3 rounded-3xl text-white w-full hover:bg-rose-900 transition-colors duration-200">Start
+        New Order</button>
+    </section>
   </section>
 </template>
