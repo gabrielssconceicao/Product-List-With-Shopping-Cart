7aa31c9219d45620a4caf6d022256b578c1bbd88
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-07-29 14:35:17 -0300

    fixing images

--- src/components/ConfirmOrder/ConfirmOrderItemComponent.vue
+++ src/components/ConfirmOrder/ConfirmOrderItemComponent.vue
@@ -4,13 +4,15 @@ type ConfirmOrderItemProps = {
   product: CartItem
 }
 const props = defineProps<ConfirmOrderItemProps>()
+
+const productThumbnail = new URL(props.product.image, import.meta.url).href;
 </script>
 
 <template>
   <div class="flex flex-row items-stretch gap-3 px-2 bg-rose-100 border-b-4 border-gray-200
   ">
     <div class="w-20">
-      <img :src="props.product.image" :alt="`${props.product.name} thumbnail`">
+      <img :src="productThumbnail" :alt="`${props.product.name} thumbnail`">
     </div>
     <div class="flex-1 flex-grow flex flex-col ">
       <p class=" px-2 py-1"> {{ props.product.name }}</p>
--- src/components/ProductCard/ProductImageComponent.vue
+++ src/components/ProductCard/ProductImageComponent.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-
 type ImageProps = {
   desktop: string;
   tablet: string;
@@ -9,18 +8,22 @@ type ImageProps = {
   isOnCart: boolean;
 };
 
+const props = defineProps<ImageProps>()
 const borderColor = computed(() => {
   return props.isOnCart ? 'border-red' : 'border-slate-300'
 })
 
-const props = defineProps<ImageProps>()
+const desktopImage = new URL(props.desktop, import.meta.url).href;
+const tabletImage = new URL(props.tablet, import.meta.url).href;
+const mobileImage = new URL(props.mobile, import.meta.url).href;
+
 </script>
 <template>
   <picture>
-    <source media="(min-width: 1440px)" :srcset="props.desktop" />
+    <source media="(min-width: 1440px)" :srcset="desktopImage" />
 
-    <source media="(max-width: 375px)" :srcset="props.mobile" />
-    <img :src="props.tablet" :alt="`${props.alt} image`"
+    <source media="(max-width: 375px)" :srcset="mobileImage" />
+    <img :src="tabletImage" :alt="`${props.alt} image`"
       class="max-mobile:w-full mobile:max-w-[200px] rounded-lg border-2 transition-colors duration-200 self-center mx-auto"
       :class="borderColor" />
   </picture>
